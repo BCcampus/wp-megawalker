d8377a23fc588d650fba3ba98c284816fb0f0085
bdolor <bpayne@bccampus.ca>
2018-11-07 15:16:51 -0800

    apply toggle class to top level only

--- inc/MegaWalker.php
+++ inc/MegaWalker.php
@@ -99,7 +99,7 @@ class MegaWalker extends \Walker_Nav_Menu {
 		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
 		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
 		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
-		$attributes .= ($args->has_children) ? ' class="dropdown-toggle parent nav-link" data-toggle="dropdown"' : 'class="nav-link"';
+		$attributes .= ($args->has_children && $depth === 0 ) ? ' class="dropdown-toggle parent nav-link" data-toggle="dropdown"' : ' class="nav-link"';
 
 		$item_output = $args->before;
 		$item_output .= '<a' . $attributes . '>';
